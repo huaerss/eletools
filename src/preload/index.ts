a783f48a81c7ec1deb842bae0a98e76f79ed0d33
huaerss <969742563@qq.com>
2024-03-04 14:27:04 +0800

    获取窗口外的信息

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,30 +1,53 @@
-import { contextBridge } from 'electron'
+import { contextBridge, clipboard } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
-import { uIOhook, UiohookKey } from 'uiohook-napi'
+import { uIOhook } from 'uiohook-napi'
+const { keyboard, Key } = require("@nut-tree/nut-js");
+
+async function performCopy() {
+  await keyboard.pressKey(Key.LeftControl, Key.C);
+  await keyboard.releaseKey(Key.LeftControl, Key.C);
+}
 
 
-let rightMouseDownTime: number
+
+let rightMouseDownTime = 0;
+let rightMouseTimer; // 用于存储定时器 ID
 
 uIOhook.on('mousedown', (e) => {
   if (e.button === 2) {
     // 记录当前时间戳
-    rightMouseDownTime = Date.now();
+    // rightMouseDownTime = Date.now();
+    // // 如果之前已经有一个计时器，先清除它
+    // if (rightMouseTimer) {
+    //   clearTimeout(rightMouseTimer);
+    // }
+    performCopy().then(
+      () => {
+        console.log('全部cv操作执行完毕')
+      }
+    )
+    setTimeout(() => {
+      console.log('复制到的内容为:', clipboard.readText())
+
+    }, 500);
+
   }
 });
 
 uIOhook.on('mouseup', (e) => {
   if (e.button === 2) {
+    // 如果鼠标右键提起，清除定时器
+    clearTimeout(rightMouseTimer);
+    rightMouseTimer = null; // 重置定时器变量
     const duration = Date.now() - rightMouseDownTime;
-    // 如果按下的时长超过500毫秒，则算作长按
-    if (duration > 400) {
-      // 获取选中的文本
-      const selectedText = window.getSelection()!.toString();
-      console.log('selectedText', selectedText);
-    }
-    // 清除记录的时间
-    rightMouseDownTime = 0
+    console.log(`右键按下了${duration} ms`);
+    // 重置按下的时间
+    rightMouseDownTime = 0;
   }
 });
+
+
+
 
 uIOhook.start()
 // Custom APIs for renderer
