65b0182ab73d8d34f79d0384e090144c6f26a571
huaerss <969742563@qq.com>
2024-03-08 13:49:38 +0800

    修复api挂掉

--- src/renderer/src/components/Versions.vue
+++ src/renderer/src/components/Versions.vue
@@ -29,38 +29,37 @@ onMounted(() => {
   window.electronAPI.onClipboardDataReceived(async (res) => {
     clipboardData.value = '翻译中...'
     gptcontentvalue.value = "查询中..."
-    if (typeof (res) === 'string') {
-      const result = await window.electron.ipcRenderer.invoke('perform-request', {
-        data: {
-          text: [res],
-          "target_lang": "ZH"
-        },
-        Headers: {
-          "Content-Type": "application/json"
-        }
-      });
-      clipboardData.value = result.data;
+    const result = await window.electron.ipcRenderer.invoke('perform-request', {
+      data: {
+        text: res,
+        "target_lang": "ZH"
+      },
+      Headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    clipboardData.value = result.data;
 
-      const gptcontent = await window.electron.ipcRenderer.invoke('GPT', {
-        data: {
-          "api_key": 'api_pjtb7Y7LdJgUAnkXRBMR',
-          "messages": [
-            {
-              "role": "user",
-              "message": `我问你所有问题你都需要中文回复我并且不需要输出你理解的代码,相关问题也要进行回答并可以回答我里面的表达是什么意思,你不需要回复我好的,你只需要回复我问的问题,那我的问题是:${res}`,
-              "codeContexts": []
-            }
-          ],
-          "chat_model": "GPT4 Turbo"
-        },
-        headers: {
-          'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyXzJkTTN5Q3BlVFlKejBGZXVNWlZQUDdGTlppdCIsImV4cCI6MTcwOTgwNTE5NH0.VI-Fyx8nyArSZX5SSjnxolWIxn2ZSFF2jRdDW4548YI`,
-          'Content-Type': 'application/json'
-        }
-      });
-      gptcontentvalue.value = gptcontent
+    const gptcontent = await window.electron.ipcRenderer.invoke('GPT', {
+      data: {
+        "api_key": 'api_qUjQQfNMhMnCqnqLnFcg',
+        "messages": [
+          {
+            "role": "user",
+            "message": `我问你所有问题你都需要中文回复我并且不需要输出你理解的代码,相关问题也要进行回答并可以回答我里面的表达是什么意思,你不需要回复我好的,你只需要回复我问的问题,那我的问题是:${res}`,
+            "codeContexts": []
+          }
+        ],
+        "chat_model": "GPT4 Turbo"
+      },
+      headers: {
+        'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ1c2VyXzJkTTN5Q3BlVFlKejBGZXVNWlZQUDdGTlppdCIsImV4cCI6MTcwOTgwNTE5NH0.VI-Fyx8nyArSZX5SSjnxolWIxn2ZSFF2jRdDW4548YI`,
+        'Content-Type': 'application/json'
+      }
+    });
+    gptcontentvalue.value = gptcontent
 
-    }
+
 
   });
 });
@@ -85,11 +84,6 @@ onUnmounted(() => {
 </template>
 
 <style>
-:root {
-  height: 100;
-  width: 500;
-}
-
 .draggable {
   -webkit-app-region: drag;
   height: 200px;
